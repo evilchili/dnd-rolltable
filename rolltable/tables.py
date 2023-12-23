edc67fed7eb6e86d087486dd8731172bb5236917
evilchili <evilchili@gmail.com>
2023-12-22 21:52:41 -0800

    fixing infinite recursion

--- rolltable/tables.py
+++ rolltable/tables.py
@@ -96,8 +96,7 @@ class DataSource:
             headers = self.headers
             for i in range(0, len(self.headers) - len(entries[0])):
                 headers.append(f"{i:3f}")
-            items = {(k, v) for k, v in zip(self.headers, self.get_entries(name))}
-            print(items)
+            items = {(k, v) for k, v in zip(self.headers, entries)
             data[name] = dict(items)
         return data
 
